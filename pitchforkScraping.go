26650c8ee9fe331596cd1edadeeec24b81c648c8
kubiak <phkubiak@gmail.com>
2015-12-10 23:35:56 -0500

    Applying switch for conditional statements, changes.

--- pitchforkScraping.go
+++ pitchforkScraping.go
@@ -29,15 +29,20 @@ func main() {
 	z := html.NewTokenizer(b)
 
 	var songInfoFieldsNext bool = false // use it to check whether TextToken follows StartTagToken
+	type entry struct {
+		artist string
+		song   string
+	}
+	//entries := make([]entry, 2)
 	for {
 		tt := z.Next()
 		//fmt.Println("tt: ",tt) // prints type of token
 		//fmt.Println(z.Token())
-		if tt == html.ErrorToken {
-			fmt.Println("Here's the error token.")
+		switch tt {
+		case html.ErrorToken:
 			fmt.Println(z.Err())
-			break // exit loop
-		} else if tt == html.StartTagToken {
+			return // exit loop
+		case html.StartTagToken:
 			currentToken := z.Token()
 			if songInfoFieldsNext == false && currentToken.Data == "h1" { // heading 1 contains artist's name
 				songInfoFieldsNext = true // next pass should be a TextToken
@@ -45,26 +50,12 @@ func main() {
 			} else if songInfoFieldsNext == true && currentToken.Data != "h2" { // next was true, but next pass isn't song name
 				songInfoFieldsNext = false
 			}
-		} else if tt == html.TextToken {
+		case html.TextToken:
 			currentToken := z.Token()
 			if songInfoFieldsNext == true { // previous pass was StartTagToken h1
-				fmt.Println("Printing artist: ", currentToken) // Artist's name
-				if currentToken.Data != "h2" {                 // not song name that follows artist name
-					continue
-				} else {
-					fmt.Println(currentToken)
-					songInfoFieldsNext = false
+				if currentToken.Data != "" {
+					fmt.Println("Printing artist: ", currentToken)
 				}
-				//fmt.Println("Next Token 1:",z.Next())
-				//fmt.Println("Next Token 2:",z.Next())
-				//fmt.Println("Token 2 Text:",z.Token())
-				//fmt.Println("Next Token 3:",z.Next())
-				//fmt.Println("Token 3 Text:",z.Token())
-				//fmt.Println("Next Token 4:",z.Next())
-				//fmt.Println("Token 4 Text:",z.Token())
-				//fmt.Println("Next Token 5:",z.Next())
-				//fmt.Println("Token 5 Text:",z.Token())
-				//fmt.Println("Next Token 6:",z.Next())
 			}
 		}
 	}
