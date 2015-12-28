9d0dd318e1b964d19c6f354f190a353a01e3531c
kubiak <phkubiak@gmail.com>
2015-12-27 23:02:12 -0500

    Now able to populate user playlist in Spotify.w00t

--- spot.py
+++ spot.py
@@ -5,19 +5,23 @@ import sys
 
 #const
 SEARCH_LIMIT = 50
-PAGE_LIMIT = 15
+PAGE_LIMIT = 15 # iterate through this number of page results
 
 
 def find_songs(user):
-    token = util.prompt_for_user_token(user)
+    token = util.prompt_for_user_token(user,scope='user-library-read')
     if token:
-        print "\nGot token."
         sp = spotipy.Spotify(token)
         songs = sp.current_user_saved_tracks(limit=10)
         print songs
-        pass
     else:
         print "Can't get %s\'s token." % (user)
+
+def add_song_to_playlist(user,song_uri,playlist_id):
+    token = util.prompt_for_user_token(user,scope='playlist-modify-public')
+    if token:
+        sp = spotipy.Spotify(token)
+        sp.user_playlist_add_tracks(user,playlist_id,song_uri)
 
 
 def get_artist_uri(artist):
@@ -40,7 +44,7 @@ def get_artists_song_uri(artist_uri, name):
     while results['next'] and limit <= PAGE_LIMIT:  # iterate through pages of results
         result = sp.next(results)
         results = result['tracks']
-        tracks.extend(results['items'])
+        tracks.extend(results['items']) # add additional results
         limit += 1
     for i in tracks:
         song_uri = i['uri']
@@ -49,11 +53,12 @@ def get_artists_song_uri(artist_uri, name):
                 return song_uri
     print "Couldn't find the track"
 
+
 if __name__ == "__main__":
     # TODO apparantly artist name is case sensitive
     # TODO leading space in songname won't work
-    if len(sys.argv) == 1:
-        print "Usage: \"Artist: Track\""
+    if len(sys.argv) < 2:
+        print "Usage: \"Artist:Track\""
         sys.exit()
     else:
         args = sys.argv[1:]
@@ -61,4 +66,4 @@ if __name__ == "__main__":
         artist, song = joined.split(":")
         artist_uri = get_artist_uri(artist)
         song_uri = get_artists_song_uri(artist_uri, song)
-        print song_uri
+    add_song_to_playlist("1223788894",[song_uri],"spotify:user:1223788894:playlist:1ckhN9MgnC7JtULrZOYCOM")
